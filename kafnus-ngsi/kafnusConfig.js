1069813ac0036465caca1577c473110e304f4b3e
GregorioBlazquez <gregorio.blazquez@estudiante.uam.es>
2026-01-20 12:28:59 +0100

    Change from fixed mongo prefix to new custom SMT prefix

--- kafnus-ngsi/kafnusConfig.js
+++ kafnus-ngsi/kafnusConfig.js
@@ -204,10 +204,6 @@ const envVarsSchema = {
         KAFNUS_NGSI_SUFFIX_TOPIC: {
             type: 'string',
             default: ''
-        },
-        KAFNUS_NGSI_MONGO_PREFIX: {
-            type: 'string',
-            default: 'sth_'
         }
     }
 };
@@ -293,9 +289,6 @@ const config = {
     ngsi: {
         prefix: envVars.KAFNUS_NGSI_PREFIX_TOPIC,
         suffix: envVars.KAFNUS_NGSI_SUFFIX_TOPIC
-    },
-    mongo: {
-        prefix: envVars.KAFNUS_NGSI_MONGO_PREFIX
     }
 };
 
