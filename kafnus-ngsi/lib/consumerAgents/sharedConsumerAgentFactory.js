9c647c8d237fcbcb30963313d42028b77e233cb6
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2026-02-11 17:27:11 +0100

    fix queue full handling

--- kafnus-ngsi/lib/consumerAgents/sharedConsumerAgentFactory.js
+++ kafnus-ngsi/lib/consumerAgents/sharedConsumerAgentFactory.js
@@ -1,21 +1,21 @@
 /*
-* Copyright 2026 Telefónica Soluciones de Informática y Comunicaciones de España, S.A.U.
-*
-* This file is part of kafnus
-*
-* kafnus is free software: you can redistribute it and/or
-* modify it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* kafnus is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
-* General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with kafnus. If not, see http://www.gnu.org/licenses/.
-*/
+ * Copyright 2026 Telefónica Soluciones de Informática y Comunicaciones de España, S.A.U.
+ *
+ * This file is part of kafnus
+ *
+ * kafnus is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * kafnus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with kafnus. If not, see http://www.gnu.org/licenses/.
+ */
 
 const Kafka = require('@confluentinc/kafka-javascript');
 const PQueue = require('p-queue').default;
@@ -30,7 +30,7 @@ function createConsumerAgent(logger, { groupId, topic, onData, producer }) {
 
     // Queue for processing: just 1 message at the same time
     const queue = new PQueue({ concurrency: 1 });
-
+    const MAX_BUFFERED_TASKS = 1000;
     let paused = false;
     let producerQueueFull = false;
 
@@ -67,20 +67,25 @@ function createConsumerAgent(logger, { groupId, topic, onData, producer }) {
                 resolve(consumer);
             })
             .on('data', (message) => {
+                if (queue.size >= MAX_BUFFERED_TASKS) {
+                    pauseConsumer();
+                    // Check: message could be already delivered; queue or discard
+                }
                 queue.add(async () => {
-                    if (producerQueueFull) {
-                        pauseConsumer();
-                        return;
-                    }
-
                     try {
                         await onData(message);
                     } catch (err) {
                         if (err.code === Kafka.CODES.ERRORS.QUEUE_FULL) {
                             producerQueueFull = true;
                             pauseConsumer();
-                        } else {
-                            logger.error(`[consumer] Processing error: %j`, err);
+                            throw err; // do not continue
+                        }
+                        logger.error(`[consumer] Processing error: %s`, err?.stack || err);
+                        throw err;
+                    } finally {
+                        // when queue is low then resume
+                        if (paused && !producerQueueFull && queue.size < MAX_BUFFERED_TASKS / 2) {
+                            resumeConsumer();
                         }
                     }
                 });
